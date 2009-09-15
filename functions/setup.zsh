a69e957b9ce723466223aa28f84428bb450cf8ef
Robert Sanders <robert@esquimaux.org>
2009-09-14 23:33:22 -0400

    lessen variable pollution

--- functions/setup.zsh
+++ functions/setup.zsh
@@ -1,6 +1,7 @@
 for file in $ZCONFIGDIR/functions/*; do
-  base=`basename $file`
-  if [ $base != "setup.zsh" ]; then
-    autoload -U $base
+  zcbase=`basename $file`
+  if [ $zcbase != "setup.zsh" ]; then
+    autoload -U $zcbase
   fi
 done
+unset $zcbase
