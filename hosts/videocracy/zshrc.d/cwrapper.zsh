f4e8f490e424d0770c927f6b5c5744656fda1d3f
Robert Sanders <robert@esquimaux.org>
2009-09-15 03:28:19 -0400

    remove noise

--- hosts/videocracy/zshrc.d/cwrapper.zsh
+++ hosts/videocracy/zshrc.d/cwrapper.zsh
@@ -1,6 +1,5 @@
 local x
 for x in /usr/local/lib/cw/*(.x)
 do
-  echo "LOCAL CW: $x"
   hash `basename $x`=$x
 done
--- lib/rsutils/librc.zsh
+++ lib/rsutils/librc.zsh
@@ -1,6 +1,5 @@
 FPATH=`dirname $0`/functions:$FPATH
 
-echo "LODIING LIB"
 local zfunc
 for zfunc in `dirname $0`/functions/*; do
   autoload -Uz `basename $zfunc`
