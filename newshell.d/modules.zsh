6d1c0c9da44381e23a05992c3f8228b520af4679
Robert Sanders <robert@esquimaux.org>
2009-09-16 19:14:00 -0400

    autoload all zsh modules

---
+++ newshell.d/modules.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+function {
+    zmodload -a zsh/stat stat zstat 
+    zmodload -a zsh/zpty zpty
+    zmodload -a zsh/zprof zprof
+    zmodload -ap zsh/mapfile mapfile
+    zmodload -a zsh/sched sched
+    zmodload -a zsh/system syserror sysread syswrite
+    zmodload -a zsh/terminfo echoti
+    zmodload -a zsh/termcap echotc
+    zmodload -a zsh/zprof zprof
+    zmodload -a zsh/zpty zpty
+    zmodload -a zsh/clone clone
+
+    zmodload zsh/datetime
+    zmodload zsh/mathfunc
+    zmodload zsh/mapfile
+    zmodload zsh/parameter
+    zmodload zsh/regex
+    zmodload zsh/zutil
+} 2>/dev/null
