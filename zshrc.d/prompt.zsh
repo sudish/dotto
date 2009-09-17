ca8077845866ce50c5566443c257ad141737b619
Robert Sanders <robert@esquimaux.org>
2009-09-16 23:48:10 -0400

    add hook functions for themes

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -1,39 +1,50 @@
-
-if [[ x$WINDOW != x ]]
-then
-    SCREEN_NO="%B$WINDOW%b "
-else
-    SCREEN_NO=""
-fi
-
-# default prompt
-PS1="%n@%m:%~%# "
+#!/bin/zsh
 
 # Setup the prompt with pretty colors
 setopt prompt_subst
 
 typeset -g -A zthemevars
 
-RPROMPT=''
+ztheme_reset() {
+    zthemevars=
+
+    for funcname in preexec precmd chpwd periodic; do
+        eval "function ztheme_${funcname} () { }"
+        typeset -g -a "${funcname}_functions"
+        set_add "${funcname}_functions" "ztheme_${funcname}"
+    done
+}
+
 ztheme() {
   if [ "$1" = "-l" ]; then
     ls -1 $ZCONFIGDIR/themes | sed -e 's/.zsh-theme$//'
   elif [ -n "$1" ]; then
     ZSH_THEME=$1
+  else
+    echo "Current theme is $ZSH_THEME"
+    return
+  fi
+  
+  local file="$ZCONFIGDIR/themes/$ZSH_THEME.zsh-theme"
+  
+  if [ -f $file ]; then
     RPROMPT=''
     PS1='%m%#  '
     PS2='%_> '
     PS3='?# '
     PS4='+%N:%i> '
-  else
-    echo "Current theme is $ZSH_THEME"
+    ztheme_reset
+    source "$ZCONFIGDIR/themes/$ZSH_THEME.zsh-theme"
   fi
-  
-  themevars=()
-  
-  source "$ZCONFIGDIR/themes/$ZSH_THEME.zsh-theme"
 }
+
 
 if [ -n "$ZSH_THEME" ]; then
   ztheme $ZSH_THEME
+else
+  ztheme_reset
 fi
+
+typeset -g -H PS1 PS2 PS3 PS4 RPS1 RPS2 PROMPT PROMPT2 PROMPT3 RPROMPT prompt
+
+
