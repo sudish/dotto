a8b33169e14a42929356785b4ff389355c7c01ef
Robert Sanders <robert@esquimaux.org>
2009-09-18 19:21:35 -0400

    hide error messages

--- zlogin.d/binpaths.zsh
+++ zlogin.d/binpaths.zsh
@@ -3,5 +3,5 @@
 local zpwd realpath tempdir
 for tempdir in $HOME/bin/paths/* $HOME/bin; do
     realpath=$(cd -P $tempdir; builtin pwd -P)
-done
+done >/dev/null 2>/dev/null
 unset zpwd realpath tempdir
