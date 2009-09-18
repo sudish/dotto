3156a9766d856900755262b75695b72e54e3f0e6
Robert Sanders <robert@esquimaux.org>
2009-09-18 05:10:55 -0400

    don't redefine shell builtins to use cwrapper

--- hosts/videocracy.esquimaux.org/zshrc.d/cwrapper.zsh
+++ hosts/videocracy.esquimaux.org/zshrc.d/cwrapper.zsh
@@ -2,7 +2,7 @@ local fpath fname
 for fpath in /usr/local/lib/cw/*(.xN)
 do
     fname=$fpath:t
-    if ! typeset -f $fname >/dev/null; then
+    if ! typeset -f $fname >/dev/null && [[ $builtins[$fname] != "defined" ]]; then
         eval "$fname() { $fpath \"\${(@)*}\" }"
     fi
 done
