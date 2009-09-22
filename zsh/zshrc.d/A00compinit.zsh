a76b7b88be2f14d37e3b92f8f3ec8f59aac97140
Robert Sanders <rsanders@gmail.com>
2009-09-22 10:15:57 -0400

    don't dump .zcompdump if we're in sudo/su mode.

--- zsh/zshrc.d/A00compinit.zsh
+++ zsh/zshrc.d/A00compinit.zsh
@@ -1,6 +1,15 @@
 #!/bin/zsh
 
+# redefine menu-select widget, other goodies
+zmodload -i zsh/complist
+
 autoload -U compinit
-compinit -C
+
+# compinit, but don't dump if we're sudo/su
+local compinit_args
+compinit_args=(-C)
+
+[[ ! -O $HOME ]] && compinit_args+=(-D)
+compinit ${compinit_args}
 
 # -*- Mode: Shell Script; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
