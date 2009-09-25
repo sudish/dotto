4c86789f7daf1785bc4401b24d7af3dabb261256
Robert Sanders <rsanders@gmail.com>
2009-09-25 03:02:27 -0400

    actually add to path

--- zsh/zlogin.d/binpaths.zsh
+++ zsh/zlogin.d/binpaths.zsh
@@ -2,6 +2,7 @@
 
 local zpwd realpath tempdir
 for tempdir in $HOME/bin/paths/* $HOME/bin; do
-    realpath=$(cd -P $tempdir; builtin pwd -P)
+  zrealpath $tempdir && realpath=$REPLY && path+=$realpath
 done >/dev/null 2>/dev/null
+
 unset zpwd realpath tempdir
