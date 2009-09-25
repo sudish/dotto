fe5ec48a4ccb4fd2effce4a5d4e92b829a097428
Robert Sanders <rsanders@gmail.com>
2009-09-25 03:50:58 -0400

    fix process completion with aliased ps, and take out about 100ms of startup delay from eager loading of .ssh/known_hosts

--- zsh/zcomp.d/ohmyzsh.zsh
+++ zsh/zcomp.d/ohmyzsh.zsh
@@ -2,17 +2,14 @@
 #setopt COMPLETE_IN_WORD
 #setopt ALWAYS_TO_END
 
-unsetopt flowcontrol
+# unsetopt flowcontrol
 
 WORDCHARS=''
-
-zmodload -i zsh/complist
 
 ## case-insensitive (all),partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
 
 unsetopt MENU_COMPLETE
 setopt AUTO_MENU
@@ -23,5 +20,8 @@ zstyle ':completion:*:*:*:*:*' menu yes select
 # zstyle ':completion:*:*:*:*:processes' force-list always
 
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
-zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
+zstyle ':completion:*:*:*:*:processes' command "command ps -u `whoami` -o pid,user,comm -w -w"
+
+### RDS - disabled for speed, and std zsh completions appear to do this just fine
+# zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
+# zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
